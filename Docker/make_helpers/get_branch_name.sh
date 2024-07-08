56ebc96af40480a26dd23da7716b8d280280f19f
Rohan Katkar <rohan.katkar@dnv.com>
2024-07-08 16:57:51 -0500

    test

--- Docker/make_helpers/get_branch_name.sh
+++ Docker/make_helpers/get_branch_name.sh
@@ -2,27 +2,8 @@
 
 # Returns branch name, regardless of whether run locally or on github runner
 
-if [[ $GITHUB_ACTIONS == "true" ]] ; then
 
-	BRANCH_PATTERN="^refs/heads/(.*)"
-	PR_PATTERN="^refs/pull/([0-9]+)/"
+BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
 
-	if [[ $GITHUB_REF =~ $BRANCH_PATTERN ]]; then
-		BRANCH_NAME=${BASH_REMATCH[1]}
-	elif [[ $GITHUB_REF =~ $PR_PATTERN ]]; then
-		PR_NUMBER=${BASH_REMATCH[1]}
-		# If not specified, assume PR comes from StatCan/aaw-kubeflow-containers
-		OWNER=${OWNER:-StatCan}
-	    REPOSITORY=${REPOSITORY:-aaw-kubeflow-containers}
-	    BRANCH_NAME=$(curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$OWNER/$REPOSITORY/pulls/$PR_NUMBER | jq '.head.ref')
-	    # Remove leading/trailing quotes
-	    BRANCH_NAME=$(sed -e 's/^"//' -e 's/"$//' <<<"$BRANCH_NAME")
-	else
-		BRANCH_NAME=""
-	fi
-
-else
-	BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
-fi
 
 echo ${BRANCH_NAME}
